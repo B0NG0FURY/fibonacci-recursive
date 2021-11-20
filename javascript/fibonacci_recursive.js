d72acd27704d82d14b809b6b06334640a5d88ad9
bongofury <jtroutmanman@gmail.com>
2021-11-20 14:31:28 -0600

    commit tree d5e1904e6c3245c7b88758c98abd5841283c9e8b

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,16 +1,19 @@
 function fibonacci(n) {
-  // type your code here
+  if (n < 2) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 0");
-  console.log("=>", fibonacci(0));
+  console.log(fibonacci(0));
 
   console.log("");
 
   console.log("Expecting: 1");
-  console.log("=>", fibonacci(2));
+  console.log(fibonacci(2));
 
   console.log("");
 
@@ -21,4 +24,6 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+
+
 // And a written explanation of your solution
